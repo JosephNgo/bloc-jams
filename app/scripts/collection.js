185d891cd9d0a79e85f88db4ea47482b504973f4
Joe Ngo <joengo83@gmail.com>
2014-09-15 23:58:59 -0700

    Added X:XX Total Length feature

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -10,6 +10,8 @@ var buildAlbumThumbnail = function() {
       + '      <br/>'
       + '      X songs'
       + '      <br/>'
+      + '      X:XX Total Length'
+      + '      <br/>'
       + '    </p>'
       + '  </div>'
       + '</div>';
