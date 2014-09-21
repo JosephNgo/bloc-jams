9ed6fc385123c11a2ee7a094dfb119a7df78c7dc
Joe Ngo <joengo83@gmail.com>
2014-09-20 22:08:43 -0700

    Added functionality to player controls

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -216,6 +216,49 @@ var changeAlbumView = function(album) {
    }
  };
 
+ //update the seek functions in the player bar
+ var updateSeekPercentage = function($seekBar, event) {
+  var barWidth = $seekBar.width();
+  var offsetX = event.pageX - $seekBar.offset().left; //get mouse x offset here
+
+  var offsetXPercent = (offsetX  / $seekBar.width()) * 100;
+  offsetXPercent = Math.max(0, offsetXPercent);
+  offsetXPercent = Math.min(100, offsetXPercent);
+
+  var percentageString = offsetXPercent + '%';
+  $seekBar.find('.fill').width(percentageString);
+  $seekBar.find('.thumb').css({left: percentageString});
+  console.log(percentageString);
+ }
+
+ var setupSeekBars = function() {
+   $seekBars = $('.player-bar .seek-bar');
+   $seekBars.click(function(event) {
+     updateSeekPercentage($(this), event);
+     console.log('x:'+ event.screenX);
+     console.log('y:'+ event.screenY);
+   });
+
+   $seekBars.find('.thumb').mousedown(function(event){
+    var $seekBar = $(this).parent();
+
+    $seekBar.addClass('no-animate');
+
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+
+    //cleanup
+    $(document).bind('mouseup.thumb', function(){
+      $seekBar.removeClass('no-animate');
+
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+
+   });
+ }; 
+
  // This 'if' condition is used to prevent the jQuery modifications
  // from happening on non-Album view pages.
  //  - Use a regex to validate that the url has "/album" in its path.
@@ -223,6 +266,7 @@ var changeAlbumView = function(album) {
    // Wait until the HTML is fully processed.
    $(document).ready(function() {
      changeAlbumView(albumMarconi);
+     setupSeekBars();
    });
  }
 });
